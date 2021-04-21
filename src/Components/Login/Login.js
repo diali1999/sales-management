6762660ac6741e88a43171655d0bfc7b99882186
Harshit Dixit <harshitdxt3004@gmail.com>
2021-04-21 15:17:25 +0530

    Login form updated

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,6 +2,8 @@ import {useState} from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
 
+import {Alert} from 'react-bootstrap';
+
 async function loginUser(credentials){
     return fetch('http://localhost:5000/api/login', {
         method: 'POST',
@@ -10,25 +12,33 @@ async function loginUser(credentials){
         },
         body: JSON.stringify(credentials)
     })
-    .then(data => data.json())
+    .then(res => res.json())
+    .then(data => data)
+    .catch(err => err);
 }
 
 function Login({setAuthToken, setUser}) {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
-
+    const [error, setError] = useState();
+    const [show, setShow] = useState(false);
     const handleSubmit = async e => {
         e.preventDefault();
         const authToken = await loginUser({
             email,
             password
         });
+        if(authToken?.error == true){
+            setError(authToken.message);
+            setShow(true);
+        }
         setAuthToken({'token': `${authToken.token}`});
         setUser({'user':`${authToken.user?.role}`});
     }
 
     return (
         <div className="login-wrapper">
+            <Alert show={show} variant="danger" onClose={() => setShow(false)} dismissible>{`${error}`}</Alert>
             <form onSubmit={handleSubmit}>
                 <h2>Sign in</h2>
                 <div className="form-group">
